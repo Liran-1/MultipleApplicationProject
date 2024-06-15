4bac20486e860a6b9721b9a3b8a646675d3aa59d
Liran <Liran.sr@gmail.com>
2024-06-16 00:21:53 +0300

    First commit. Added images and icons. Drawer Menu in progress.

---
+++ common/src/main/java/com/example/common/Activity_Main.java
@@ -0,0 +1,15 @@
+package com.example.common;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        
+    }
+}
